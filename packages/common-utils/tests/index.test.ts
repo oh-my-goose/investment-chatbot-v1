2b69569311b6605e8adde58a8d873e35a3b118e6
Mike Chen <chintown@amazon.com>
2023-10-03 21:55:46 -0700

    test: add mocking in getApiKeySafely i:18

--- packages/common-utils/tests/index.test.ts
+++ packages/common-utils/tests/index.test.ts
@@ -1,7 +1,19 @@
 import { getApiKeySafely } from '../src';
 
 describe(`getApiKeySafely`, () => {
+    let saved = '';
+    beforeAll(() => {
+        saved = process.env.OPENAI_API_KEY || '';
+    });
+    afterAll(() => {
+        process.env.OPENAI_API_KEY = saved;
+    });
     it(`should fail if environment is not given`, () => {
+        process.env.OPENAI_API_KEY = '';
         expect(() => getApiKeySafely()).toThrow();
+    });
+    it(`should resolve if environment is given`, () => {
+        process.env.OPENAI_API_KEY = 'key';
+        expect(getApiKeySafely()).toBe('key');
     });
 });
